0cc4f1ad64321e873dbfda4761d47ebf8fea6d92
Raheemspb <gabibovda@gmail.com>
2024-07-04 15:35:05 +0300

    Import SnapKit and RealmSwift. Create RealmClass

---
+++ RickRM/RickRM/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  RickRM
+//
+//  Created by Рахим Габибли on 04.07.2024.
+//
+
+import UIKit
+import SnapKit
+import RealmSwift
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
